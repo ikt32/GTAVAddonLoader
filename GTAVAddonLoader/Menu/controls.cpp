16d179d3292bdc50cfb40f3ad48970cb2f8ad316
E66666666 <emily66666666ok@outlook.com>
2017-05-06 02:16:10 +0200

    Update menu

--- GTAVAddonLoader/Menu/controls.cpp
+++ GTAVAddonLoader/Menu/controls.cpp
@@ -1,6 +1,18 @@
 #include "controls.h"
 #include "keyboard.h"
+#include "inc/enums.h"
+#include "inc/natives.h"
 
+long long milliseconds_now() { // static
+	LARGE_INTEGER s_frequency; // static
+	BOOL s_use_qpc = QueryPerformanceFrequency(&s_frequency); // static
+	if (s_use_qpc) {
+		LARGE_INTEGER now;
+		QueryPerformanceCounter(&now);
+		return (1000LL * now.QuadPart) / s_frequency.QuadPart;
+	}
+	return GetTickCount64();
+}
 
 MenuControls::MenuControls() {
 	std::fill(controlPrev, std::end(controlPrev), false);
@@ -10,25 +22,54 @@ MenuControls::MenuControls() {
 
 MenuControls::~MenuControls() { }
 
-bool MenuControls::IsKeyPressed(int key) {
-	if (IsKeyDown(key))
-		return true;
-	return false;
+bool MenuControls::IsKeyPressed(ControlType control) {
+	return IsKeyDown(ControlKeys[control]);
 }
 
 bool MenuControls::IsKeyJustPressed(ControlType control) {
-	int key = ControlKeys[control];
-	if (IsKeyDown(key))
-		controlCurr[control] = true;
-	else
-		controlCurr[control] = false;
-
-	// raising edge
-	if (controlCurr[control] == true && controlPrev[control] == false) {
-		controlPrev[control] = controlCurr[control];
+	if (controlCurr[control] && !controlPrev[control]) {
 		return true;
 	}
+	return false;
+}
 
-	controlPrev[control] = controlCurr[control];
+bool MenuControls::IsKeyJustReleased(ControlType control) {
+	if (!controlCurr[control] && controlPrev[control]) {
+		return true;
+	}
+	return false;
+}
+
+bool MenuControls::IsKeyDownFor(ControlType control, int millis) {
+	if (IsKeyJustPressed(control)) {
+		pressTime[control] = milliseconds_now();
+	}
+
+	if (IsKeyPressed(control) && (milliseconds_now() - pressTime[control]) >= millis) {
+		return true;
+	}
+	return false;
+}
+
+void MenuControls::Update() {
+	for (int i = 0; i < SIZEOF_ControlType; i++) {
+		controlPrev[i] = controlCurr[i];
+		controlCurr[i] = IsKeyDown(ControlKeys[i]);
+	}
+	// Size of eControl
+	for (int i = 0; i < eControlSize; i++) {
+		nControlPrev[i] = nControlCurr[i];
+		nControlCurr[i] = CONTROLS::IS_DISABLED_CONTROL_PRESSED(0, i) != 0;
+	}
+}
+
+bool MenuControls::IsControlDownFor(eControl control, int millis) {
+	if (CONTROLS::IS_DISABLED_CONTROL_JUST_PRESSED(0, control)) {
+		nPressTime[control] = milliseconds_now();
+	}
+
+	if (CONTROLS::IS_DISABLED_CONTROL_PRESSED(0, control) && (milliseconds_now() - nPressTime[control]) >= millis) {
+		return true;
+	}
 	return false;
 }
