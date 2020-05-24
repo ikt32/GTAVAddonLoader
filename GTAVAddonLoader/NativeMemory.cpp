57f0662805b2a4f7328df809ccc41f33467450f9
E66666666 <emily66666666ok@outlook.com>
2020-05-24 14:19:52 +0200

    Fix compatiblity for 1.0.1868.4

--- GTAVAddonLoader/NativeMemory.cpp
+++ GTAVAddonLoader/NativeMemory.cpp
@@ -59,17 +59,35 @@ void removeHooks() {
 }
 
 void MemoryAccess::Init() {
-    auto addr = FindPattern("\x0F\xB7\x05\x00\x00\x00\x00"
-        "\x45\x33\xC9\x4C\x8B\xDA\x66\x85\xC0"
-        "\x0F\x84\x00\x00\x00\x00"
-        "\x44\x0F\xB7\xC0\x33\xD2\x8B\xC1\x41\xF7\xF0\x48"
-        "\x8B\x05\x00\x00\x00\x00"
-        "\x4C\x8B\x14\xD0\xEB\x09\x41\x3B\x0A\x74\x54",
-        "xxx????xxxxxxxxxxx????"
-        "xxxxxxxxxxxxxx????xxxxxxxxxxx");
-    if (!addr) {
-        logger.Write(ERROR, "Couldn't find GetModelInfo");
+    uintptr_t addr;
+
+    if (gameVersion <= 56) {
+        addr = FindPattern(
+            "\x0F\xB7\x05\x00\x00\x00\x00"
+            "\x45\x33\xC9\x4C\x8B\xDA\x66\x85\xC0"
+            "\x0F\x84\x00\x00\x00\x00"
+            "\x44\x0F\xB7\xC0\x33\xD2\x8B\xC1\x41\xF7\xF0\x48"
+            "\x8B\x05\x00\x00\x00\x00"
+            "\x4C\x8B\x14\xD0\xEB\x09\x41\x3B\x0A\x74\x54",
+            "xxx????"
+            "xxxxxxxxx"
+            "xx????"
+            "xxxxxxxxxxxx"
+            "xx????"
+            "xxxxxxxxxxx");
+
+        if (!addr) {
+            logger.Write(ERROR, "Couldn't find GetModelInfo");
+        }
     }
+    else {
+        addr = FindPattern("\xEB\x09\x41\x3B\x0A\x74\x54", "xxxxxxx");
+        if (!addr) {
+            logger.Write(ERROR, "Couldn't find GetModelInfo (v57+)");
+        }
+        addr = addr - 0x2C;
+    }
+
     GetModelInfo = (GetModelInfo_t)(addr);
 
     // find enable MP cars patterns
