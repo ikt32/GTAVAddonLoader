5fb3ecc6b6bbdf7aa67f58878e8ea1ab43f357f4
E66666666 <6639510+E66666666@users.noreply.github.com>
2021-10-17 14:28:00 +0200

    Bump version

--- GTAVAddonLoader/script.h
+++ GTAVAddonLoader/script.h
@@ -1,12 +1,6 @@
-/*
-THIS FILE IS A PART OF GTA V SCRIPT HOOK SDK
-http://dev-c.com
-(C) Alexander Blade 2015
-*/
-
 #pragma once
 
-#define DISPLAY_VERSION "v1.4.5"
+#define DISPLAY_VERSION "v1.5.0"
 
 #include <string>
 #include <vector>
